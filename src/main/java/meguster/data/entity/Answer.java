bfc080d65acb0e0cd5c95780e37b02e0ae434f32
p-pawel <p-pawel@users.noreply.github.com>
2015-06-13 17:31:17 +0200

    Sequence fix

--- src/main/java/meguster/data/entity/Answer.java
+++ src/main/java/meguster/data/entity/Answer.java
@@ -1,16 +1,20 @@
 package meguster.data.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Answer {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(name="pk_sequence",sequenceName="answer_id_seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="pk_sequence")
+	@Column(name="id", unique=true, nullable=false)	
 	private Long id;
 
 	@ManyToOne
