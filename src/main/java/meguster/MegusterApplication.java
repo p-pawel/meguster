7108c2e022d3e7f0e6f44423977624206c0969be
p-pawel <p-pawel@users.noreply.github.com>
2015-06-13 12:12:19 +0200

    Initial commit

---
+++ src/main/java/meguster/MegusterApplication.java
@@ -0,0 +1,12 @@
+package meguster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegusterApplication.class, args);
+    }
+}
